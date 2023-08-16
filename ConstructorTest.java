2facd810412d1d28c0bddf26ec017e72e6c88dc0
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-08-16 14:58:56 +0530

    Three

---
+++ ConstructorTest.java
@@ -0,0 +1,15 @@
+
+public class ConstructorTest {
+
+	public static void main(String[] args) {
+		//create 3 Employee type objects
+		Employee2 emp1 = new Employee2();
+		Employee2 emp2 = new Employee2();
+		Employee2 emp3 = new Employee2();
+		
+		emp1.displayProperties();
+		emp2.displayProperties();
+		emp3.displayProperties();
+	}
+
+}
