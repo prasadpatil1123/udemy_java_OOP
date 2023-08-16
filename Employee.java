3797dbd04a5e4db8e984da832419abe34bf3b8cf
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-08-16 12:32:58 +0530

    first commit

---
+++ Employee.java
@@ -0,0 +1,13 @@
+class Employee
+{
+	int empno;
+	String ename;
+	float sal;
+
+	public static void main(String[] args){
+
+	Employee emp = new Employee();
+	System.out.println(emp.empno + " | "+ emp.ename + " | "+ emp.sal);
+
+	}
+}
